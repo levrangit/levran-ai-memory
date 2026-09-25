e29a0662e64dd4ffc527b9204f6e77f1a2a8a6f2
levran <levran.mail@gmail.com>
2026-09-25 23:29:49 +0500

    Ensure PowerShell 5.1 BOM

--- src/update.ps1
+++ src/update.ps1
@@ -1,4 +1,4 @@
-#Requires -Version 5.1
+﻿#Requires -Version 5.1
 # Если PowerShell запрещает запуск скрипта, выполните:
 # Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
 # Проверить текущую политику:
